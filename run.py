32cf73ce0bc1f83dfc8eb3fad99d96d1268ab785
ezgor <kshkrve@gmail.com>
2022-05-10 16:07:14 +0300

    Updatet

--- run.py
+++ run.py
@@ -1,20 +1,25 @@
 import sys
-from codes.GAN_class import DownsampleGAN
+#from codes.GAN_class import DownsampleGAN
 from  codes import my_utils
 import cv2 as cv
 import torch
 import os
+from tqdm import tqdm
 
 ###
-# python3 run.py ./weights/checkpoints/netG_9  ../test1 ../test1_dws
+# python3 run.py ./saved_weights/models/mod_19  [inputs folder path] [output folder path] [size of block to cut the image; default 256]
+# python3 run.py ./saved_weights/models/mod_19  ../test1/result_parts/part1 ../test1/result_parts_dws/part1 1024
 ###
 
 if __name__ == "__main__":
     print('torch.cuda.is_available:', torch.cuda.is_available())
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print('device:', device)
-    pathes = {'generator': sys.argv[1], 'load': sys.argv[2], 'save': sys.argv[3]}
-    generator = my_utils.create_generator(pathes['generator'], device)
+    pathes = {'model': sys.argv[1], 'load': sys.argv[2], 'save': sys.argv[3]}
+    block_size = 256 if not sys.argv[4] else int(sys.argv[4])
+    #generator = my_utils.create_generator(pathes['generator'], device)
+    dwsGAN = my_utils.load_model(pathes['model'])
+
     if not os.path.exists(pathes['save']):
         os.mkdir(pathes['save'])
         
@@ -29,15 +34,20 @@ if __name__ == "__main__":
             files[0].append(file_load)
             files[1].append(file_save)
     files[0].sort()
-
+    files[1].sort()
     
-    for path_load, path_save in zip(files[0], files[1]):
-        img = my_utils.read_img(path_load)
+    print('Start image degrading ...')
+    
+    count = 0	
+
+    for path_load, path_save in tqdm(zip(files[0], files[1])):
         with torch.no_grad():
             torch.cuda.empty_cache() 
-            input = img.to(device)
-            downsampled_img = my_utils.get_out(generator(input).detach().cpu().numpy()[0])
-            cv.imwrite(path_save, downsampled_img) 
-    
+            downsampled_img = my_utils.frame_processing(dwsGAN.generator, dwsGAN.device, path_load, block_size=block_size)
+            cv.imwrite(path_save, downsampled_img[..., ::-1] * 255) 
+        count += 1
+        #break #to test single frame
+
+    print(f'Successfully processed {count} frames')        
 
  
